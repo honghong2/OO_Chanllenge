e7a2c6268d23f55371acbab293baa8424a8cf870
Hong Hong <v-honghong@microsoft.com>
2022-09-15 13:16:23 -0700

    new js file

---
+++ OO_Challenge.js
@@ -0,0 +1,53 @@
+class Vehicle {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    honk() {
+        return 'Beep.';
+    }
+
+    toString() {
+        return `This vehicle is a ${this.make} ${this.model} from ${this.year}.`; 
+    }
+}
+
+class Car extends Vehicle {
+    constructor(make, model, year)
+    {
+        super(make, model, year);
+        this.numWheels = 4;
+    }
+}
+
+class Motorcycle extends Vehicle {
+    constructor(make, model, year) {
+        super(make, model, year);
+        this.numWheels = 4;
+    }
+
+    revEngine() {
+        return "VROOM!!!";
+    }
+}
+
+class Garage {
+    constructor(capacity) {
+        this.vehicles = [];
+        this.capacity = capacity;
+    }
+
+    add(newVehicle) {
+        if (!(newVehicle instanceof Vehicle)) {
+            return 'Only Vehicles are allowed in here!';
+        }
+
+        if (this.vehicles.length >= this.capacity) {
+            return "Sorry, we're full.";
+        }
+        this.vehicles.push(newVehicle);
+        return "Vehicle added!";
+    }
+}
